50a44865309a0d4ec22e783993fb4b137efb23bd
elden <elden@vlending.co.kr>
2023-02-02 16:19:49 +0900

    프로젝트 등록

---
+++ Example/Pods/Target Support Files/Pods-VlendingAds_Tests/Pods-VlendingAds_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_VlendingAds_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_VlendingAds_Tests
+@end
---
+++ Example/Pods/Target Support Files/VlendingAds/VlendingAds-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_VlendingAds : NSObject
+@end
+@implementation PodsDummy_VlendingAds
+@end
